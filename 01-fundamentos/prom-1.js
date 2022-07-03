38951f2ce299f2bad9066e5c54f810c2ad30df88
Yunen Ramos Ramírez <yunencr@outlook.com>
2022-07-02 18:21:50 -0600

    Ejemplo sobre Infierno de Callback

--- 01-fundamentos/prom-1.js
+++ 01-fundamentos/prom-1.js
@@ -1,9 +1,35 @@
 function sumarUno(elNumero, laFuncionALlamar){
+    if(elNumero >= 7){
+        laFuncionALlamar('Número muy alto');
+        return;
+    }
+
     setTimeout(function() {
-        laFuncionALlamar(elNumero + 1);
+        laFuncionALlamar(null, elNumero + 1);
     }, 800);
 }
 
-sumarUno(5, function(elNuevoValor){
-    console.log(elNuevoValor); 
+sumarUno(5, function(elMensajeDeError, elNuevoValor){
+    if(elMensajeDeError){
+        console.log(elMensajeDeError);
+        return;
+    }
+
+    sumarUno(elNuevoValor, function(elMensajeDeError, elNuevoValorDerivado){
+
+        if(elMensajeDeError){
+            console.log(elMensajeDeError);
+            return;
+        }
+
+        sumarUno(elNuevoValorDerivado, function(elMensajeDeError, elNuevoValorDerivadoDeSegundoNivel){
+
+            if(elMensajeDeError){
+                console.log(elMensajeDeError);
+                return;
+            }
+
+            console.log(elNuevoValorDerivadoDeSegundoNivel); 
+        });
+    });
 });
